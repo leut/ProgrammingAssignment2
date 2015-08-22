d124dfc2a71266ababf9a5c7e730dd4933c9dfa2
leut <profleut@gmail.com>
2015-08-22 15:18:51 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function creates a "cached matrix", which caches the 
+## not only the matrix, var "x", but also its inverse, var "cmInv"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cmInv <- NULL
+    set <- function(nm) {     # store the new matrix and set cmInv to NULL
+             # NOTE: cacheSolve(x) must later be called to set cmInv
+        x <<- nm
+        cmInv <<- NULL
+    }
+    get <- function() x    # simply return the matrix
+    setInv <- function(newInv) cmInv <<- newInv
+    getInv <- function() cmInv
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
+    
+ 
 }
 
 
-## Write a short comment describing this function
+## This calculates the inverse of matrix "x".
+## It first checks to see if there is a cached solution, if not 
+## it then solves for the inverse and sets the cached solution.
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if (!is.null(inv)) {     # if a cached version exists, return it
+        message("getting cached inverse of matrix")
+        return(inv)
+    }
+    # if not cached solutin exits, solve at set cached solution
+    tempMat <- x$get()
+    inv <- solve(tempMat)
+    x$setInv(inv)
+    inv
 }
+
+
+## to call/use, see commented out calls below
+# aMatrix <- makeCacheMatrix()
+# aMatrix$set( matrix(c(4,3,3,2),2,2,byrow=TRUE)  )
+# aMatrix$getInv()   # should return null because cacheInverse() not yet called
+# cacheInverse(aMatrix)
+# aMatrix$getInv()
+
+
+
+
+
+
